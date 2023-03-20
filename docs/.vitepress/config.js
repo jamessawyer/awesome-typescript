e0f11a66fce7c4cf2bd50a60bbb6a9edf375a004
jinx <550961784@qq.com>
2023-03-20 10:37:42 +0800

    book(ts体操类型): satisfies操作符融合自动推导和手动声明的优点

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -203,6 +203,7 @@ const sidebar_book = {
         { text: '11.类型体操顺口溜', link: '/book/master-ts/11' },
         { text: '16.TS新语法：infer extends🍭', link: '/book/master-ts/16' },
         { text: '17.原理篇：逆变、协变、双向协变、不变👩‍🏫', link: '/book/master-ts/17' },
+        { text: '27.TS新语法：satisfies推导🍭', link: '/book/master-ts/27' },
       ]
     }
   ]
